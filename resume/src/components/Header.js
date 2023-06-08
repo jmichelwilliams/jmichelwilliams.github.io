1ee2b1f77d3af2f9b7678dc54ef15326340905b7
James Michel-Williams <jmichelwilliams@gmail.com>
2023-06-08 00:59:29 -0400

    Added react-icons

--- resume/src/components/Header.js
+++ resume/src/components/Header.js
@@ -1,17 +1,31 @@
 import styled from "styled-components";
+import profilePic from "../images/JamesProfilePicture.jpeg";
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
 
 const Header = () => {
   return (
     <div>
-      <div>profile pic</div>
       <div>
-        <ul>
-          <li>link1</li>
-          <li>link2</li>
-          <li>link3</li>
-        </ul>
+        <img src={profilePic}></img>
+      </div>
+      <div>
+        <List>
+          <li>
+            <FiGithub />
+          </li>
+          <li>
+            <FiLinkedin />
+          </li>
+          <li>
+            <FiMail />
+          </li>
+        </List>
       </div>
     </div>
   );
 };
+
+const List = styled.ul`
+  list-style-type: style none;
+`;
 export default Header;
