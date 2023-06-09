7ae64a94c3f1f2a3c1330e196b93daeec3999972
James Michel-Williams <jmichelwilliams@gmail.com>
2023-06-08 20:57:03 -0400

    About Completed

---
+++ resume/src/components/About.js
@@ -0,0 +1,31 @@
+import { styled } from "styled-components";
+
+const About = () => {
+  return (
+    <Wrapper>
+      <StyledTitle>About</StyledTitle>
+      <StyledParagraph>
+        Junior front-end developper looking for work! Recently discovered my
+        passion for coding though a coding bootcamp at Concordia University. I
+        have a strong drive to learn. Currently looking to start my carreer in
+        web developpement.{" "}
+      </StyledParagraph>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledTitle = styled.h3`
+  font-size: 36px;
+  margin-bottom: 0px;
+`;
+
+const StyledParagraph = styled.p`
+  font-size: 20px;
+  line-height: 28px;
+`;
+export default About;
