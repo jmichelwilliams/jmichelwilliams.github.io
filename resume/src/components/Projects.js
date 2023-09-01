daeeb2390a615c0e94c29079543476a0adaa170d
James Michel-Williams <jmichelwilliams@gmail.com>
2023-09-01 19:00:46 -0400

    updated with project info

--- resume/src/components/Projects.js
+++ resume/src/components/Projects.js
@@ -1,11 +1,20 @@
-import styled from "styled-components";
-import Wrapper from "./Wrapper";
+import styled from 'styled-components';
+import Wrapper from './Wrapper';
+import projectPic from '../images/Homepage.png';
 
 const Projects = () => {
   return (
     <Wrapper animationDelay={1}>
       <StyledTitle id="projects">Projects</StyledTitle>
-      <p>Projects to come!</p>
+      <ProjectContainer>
+        <StyledProjectTitle>GardenHero</StyledProjectTitle>
+        <Link href="https://garden-hero.vercel.app/">
+          <ProjectImage src={projectPic} alt="GardenHero" />
+        </Link>
+        <Link href="https://github.com/jmichelwilliams/FinalProject-GardenHero">
+          Link to Repo
+        </Link>
+      </ProjectContainer>
     </Wrapper>
   );
 };
@@ -13,5 +22,24 @@ const StyledTitle = styled.h2`
   font-size: 36px;
   margin-bottom: 0px;
 `;
+const StyledProjectTitle = styled.h3`
+  font-weight: bold;
+  font-size: 24px;
+  margin-bottom: 16px;
+`;
 
+const Link = styled.a`
+  color: white;
+  font-size: 18px;
+  margin-top: 8px;
+`;
+const ProjectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const ProjectImage = styled.img`
+  height: 200px;
+  width: 400px;
+`;
 export default Projects;
